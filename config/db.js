ae686057ce6e9dfef150f453ff71ec88dc20337a
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2025-10-28 23:16:07 +0600

    db_setup

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
